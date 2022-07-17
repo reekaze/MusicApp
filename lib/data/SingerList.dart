1318688c25998fe523bb86dd6f8b04b71048d104
Your Name <you@example.com>
2022-07-18 04:20:01 +0700

    first commit

---
+++ lib/data/SingerList.dart
@@ -0,0 +1,12 @@
+class SingerList {
+  final getData = [
+    {"name": "AKMU", "album": "Play", "image": "Akmu.jpg"},
+    {"name": "Bolbbalgan4", "album": "Red Planet", "image": "Bol4.jpg"},
+    {"name": "IU", "album": "Palette", "image": "IU.png"},
+  ];
+  final getSong = [
+    {"name": "AKMU", "title": "Dinosaur", "image": "AKMU - Dinosaur.jpg"},
+    {"name": "Bolbbalgan4", "title": "Some", "image": "BOL4 - some.jfif"},
+    {"name": "IU", "title": "Coin", "image": "IU - Coin.jpg"}
+  ];
+}
